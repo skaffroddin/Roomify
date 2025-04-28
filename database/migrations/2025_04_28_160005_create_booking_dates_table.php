098a10fe1feba516dd8f9bbd5da4343302a5731b
skaffroddin <skaffroddin4@gmail.com>
2025-04-28 21:44:12 +0530

    db has been created migration

--- database/migrations/2025_04_28_160005_create_booking_dates_table.php
+++ database/migrations/2025_04_28_160005_create_booking_dates_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $table->id();
-        $table->string('name');
-        $table->string('email')->unique();
-        $table->string('password');
-        $table->string('phone')->nullable();
-        $table->enum('role', ['user', 'admin'])->default('user');
-        $table->timestamps();
+        Schema::create('booking_dates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained()->onDelete('cascade');
+            $table->date('booked_date');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('booking_dates');
     }
 };
